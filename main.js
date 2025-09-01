e321f4127a9477148e98d5144cc5264e6277c204
rhinoandre <rhinoandre@gmail.com>
2025-09-01 04:49:07 -0300

    feat: update toggle to play/reverse timelines from GSAP

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import './style.css'
 import FileIcon from './src/assets/file.svg?raw'
 import ButtonLoading from './src/assets/button-loading.svg?raw'
+import CylinderBox from './src/assets/cilinder-box.svg?raw'
+import { animateCylinderBox } from './src/assets/scripts/cilinder-box.js'
 
 const svgFiles = [
   FileIcon,
   ButtonLoading,
+  CylinderBox,
 ];
 
 const container = document.querySelector('.container');
@@ -22,10 +25,15 @@ for (const svg of svgFiles) {
   }
 }
 
-
+let toggleState = false;
+let timelines = [animateCylinderBox()];
 function toggle() {
   const contentLoader = document.querySelector('.content-loader');
   contentLoader.classList.toggle('stop');
+
+  // Start/reverse all timelines
+  timelines.forEach(timeline => toggleState ? timeline.reverse() : timeline.play());
+  toggleState = !toggleState;
 }
 
 // Add event listener to the toggle button
