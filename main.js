2a1807f803ebb4b9550014331e17f88c7dfed5d1
rhinoandre <rhinoandre@gmail.com>
2025-09-01 04:45:47 -0300

    feat: toggle animations

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 import './style.css'
 import FileIcon from './src/assets/file.svg?raw'
+import ButtonLoading from './src/assets/button-loading.svg?raw'
 
 const svgFiles = [
   FileIcon,
+  ButtonLoading,
 ];
 
 const container = document.querySelector('.container');
 for (const svg of svgFiles) {
   if (container) {
-    container.innerHTML = svg;
+    container.innerHTML += svg;
 
   // Add event listeners to the loaded SVG
   const svgElement = container.querySelector('svg');
@@ -18,4 +20,21 @@ for (const svg of svgFiles) {
     });
     }
   }
+}
+
+
+function toggle() {
+  const contentLoader = document.querySelector('.content-loader');
+  contentLoader.classList.toggle('stop');
+}
+
+// Add event listener to the toggle button
+const toggleBtn = document.querySelector('#toggle-btn');
+if (toggleBtn) {
+  toggleBtn.addEventListener('click', toggle);
+}
+
+const pathElement = document.querySelector('.check');
+if (pathElement) {
+  console.log(pathElement.getTotalLength());
 }
